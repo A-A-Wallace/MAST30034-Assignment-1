6289b07ba25176c5431a81dc9b15b519404c69a4
Angus Wallace <angus.a.wallace@gmail.com>
2021-08-26 15:43:41 +1000

    Spatial matrix added

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # MAST30034-Assignment-1
+
+The code should be run from the Mast30034-Assignment-1 folder using the following command:
+  rscript code/main.R
+
+Dependencies:
+  R libraries:
+    - corrplot
--- code/main.R
+++ code/main.R
@@ -1,4 +1,5 @@
 # Generate Time Course Matrix
+
 TCdata = c()
 AV = c(0,20,0,0,0,0)
 IV = c(30,45,60,40,40,40)
@@ -34,6 +35,7 @@ for (c in 1:length(AV)){
 
 TC = matrix(data = TCdata, nrow = 240, ncol = 6)
 
+
 # Standardisation
 
 for (c in 1:6){
@@ -43,7 +45,9 @@ for (c in 1:6){
   TC[,c] = TC[,c] / std
 }
 
+
 # Plotting
+
 png(filename = "plots/TC1plot.png")
 plot(x = 1:240, y = TC[,1], type = "l")
 dev.off()
@@ -64,15 +68,39 @@ plot(x = 1:240, y = TC[,6], type = "l")
 dev.off()
 
 
-# 
+# TC Correlation Plot
+library("corrplot")
+
+png(filename = "plots/TCCorrelationPlot.png")
+corrplot(cor(TC))
+dev.off()
 
 
+# Spatial Matrix
 
+SMdata = c()
+xmin = c(2, 2, 8, 8, 15, 15)
+xmax = c(6, 6, 13, 13, 19, 19)
+ymin = c(2, 15, 2, 15, 2, 15)
+ymax = c(6, 19, 6, 19, 6, 19)
+for (i in 1:6){
+  M = matrix(0, 21, 21)
+  for (x in xmin[i]:xmax[i]){
+    for (y in ymin[i]:ymax[i]){
+      M[x, y] = 1
+    }
+  }
+  SMdata = append(SMdata, unlist(as.list(M)))
+}
 
+SM = matrix(data = SMdata, nrow = 441, ncol = 6)
+SM = t(SM)
 
+# SM Correlation Plot
 
-
-
+png(filename = "plots/SMCorrelationPlot.png")
+corrplot(cor(t(SM)))
+dev.off()
 
 
 
